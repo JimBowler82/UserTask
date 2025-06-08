cab3fc0d1e97b79ed8cacac67ff78407301e3d10
James Bowler <james.bowler@vigosoftware.com>
2025-06-08 19:12:58 +0100

    Add indexes to User entity for improved query performance

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -15,7 +15,45 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.First_name)
+            .HasDatabaseName("IX_User_First_name");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Last_name)
+            .HasDatabaseName("IX_User_Last_name");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .HasDatabaseName("IX_User_Email");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Comments)
+            .HasDatabaseName("IX_User_Comments");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Title)
+            .HasDatabaseName("IX_User_Title");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Registration_dttm)
+            .HasDatabaseName("IX_User_Registration_dttm");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Gender)
+            .HasDatabaseName("IX_User_Gender");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Country)
+            .HasDatabaseName("IX_User_Country");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Salary)
+            .HasDatabaseName("IX_User_Salary");
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Birthdate)
+            .HasDatabaseName("IX_User_Birthdate");
 
     }
 
---
+++ Persistence/Migrations/20250608181020_AddIndexesToUser.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Birthdate",
+                table: "Users",
+                column: "Birthdate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Comments",
+                table: "Users",
+                column: "Comments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Country",
+                table: "Users",
+                column: "Country");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_First_name",
+                table: "Users",
+                column: "First_name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Gender",
+                table: "Users",
+                column: "Gender");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Last_name",
+                table: "Users",
+                column: "Last_name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Registration_dttm",
+                table: "Users",
+                column: "Registration_dttm");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Salary",
+                table: "Users",
+                column: "Salary");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Title",
+                table: "Users",
+                column: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_User_Birthdate",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Comments",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Country",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_First_name",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Gender",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Last_name",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Registration_dttm",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Salary",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_Title",
+                table: "Users");
+        }
+    }
+}
